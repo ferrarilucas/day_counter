0a3ef7f9cc3910347fb24c19cf9e63d75acf2c12
Lucas Ferrari Ribeiro <ferrari.lucasr@gmail.com>
2021-09-28 13:03:08 -0300

    Add principal screen and theme

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+
+localStorage.setItem('date_save', '20220320')
+const counter_save = localStorage.getItem('date_save')
+
+let choice = `<h2>CONTADOR</h2>\n
+<b class="d-block mb-3">Escolha uma opção</b>\n
+<button type="button" class="btn btn-success mb-3">Continuar na ultima contagem</button>\n
+<button type="button" class="btn btn-secondary mb-3">nova contagem</button>\n` 
+
+const main = document.querySelector('.principal')
+
+if (counter_save)
+    main.innerHTML =choice;
+
